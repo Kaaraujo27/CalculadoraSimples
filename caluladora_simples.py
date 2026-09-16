5df614f5b47ea2334aa63993b846051d71ef62cd
Kayque Araujo <kayquearaujo384@gmail.com>
2026-09-16 14:22:55 -0300

    Update caluladora_simples.py

--- caluladora_simples.py
+++ caluladora_simples.py
@@ -1,36 +1,33 @@
-# CALCULADORA SIMPLES
-# Atualizado com WHILE ao invés de ser somente IF, ELIF ou ELSE.
+# CALCULADORA
 
 def calculadora():
     print("Calculadora Simples")
     print("Operações: +, -, *, /")
-    continuar = "s"
 
-    while continuar == "s":
-        try:
-            primeiro = float(input("Primeiro número:R$ "))
-            operacao = input("Operação: ").strip()
-            segundo = float(input("Segundo número:R$ "))
+    try:
+        primeiro = float(input("Primeiro número:R$ "))
+        operacao = input("Operação: ").strip()
+        segundo = float(input("Segundo número:R$ "))
 
-            if operacao == "+":
-                resultado = primeiro + segundo
-            elif operacao == "-":
-                resultado = primeiro - segundo
-            elif operacao == "*":
-                resultado = primeiro * segundo
-            elif operacao == "/":
-                if segundo == 0:
-                    print("Erro: não é possível dividir por zero.")
-                    return
-                resultado = primeiro / segundo
-            else:
-                print("Operação inválida.")
-                continue
+        if operacao == "+":
+            resultado = primeiro + segundo
+        elif operacao == "-":
+            resultado = primeiro - segundo
+        elif operacao == "*":
+            resultado = primeiro * segundo
+        elif operacao == "/":
+            if segundo == 0:
+                print("Erro: não é possível dividir por zero.")
+                return
+            resultado = primeiro / segundo
+        else:
+            print("Operação inválida.")
+            return
 
-            print(f"Resultado:R$ {resultado}")
-        except ValueError:
-            print("Erro: informe números válidos.")
+        print(f"Resultado:R$ {resultado}")
 
-        continuar = input("Deseja fazer outro cálculo? (s/n): ").strip().lower()
+    except ValueError:
+        print("Erro: insira apenas números válidos.")
+
 
 calculadora()
